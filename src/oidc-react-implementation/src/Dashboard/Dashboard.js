7d68ba8c0d84954bbbd1fa6fe18dffd31bb814b6
Emrul Kayes <emrul@blockbonds.io>
2024-01-10 14:42:46 +0600

    update dashboard design

--- src/oidc-react-implementation/src/Dashboard/Dashboard.js
+++ src/oidc-react-implementation/src/Dashboard/Dashboard.js
@@ -1,18 +1,31 @@
-import { useAuth } from "oidc-react";
-import { Button } from "antd";
+import { useAuth } from 'oidc-react';
+import { Button, Col, Row } from 'antd';
 
 export default (Dashboard) => {
-    const auth = useAuth();
+  const auth = useAuth();
 
-    const handleLogout = () => {
-        auth.userManager.removeUser();
-        auth.signOutRedirect()
-      };
+  const handleLogout = () => {
+    auth.userManager.removeUser();
+    auth.signOutRedirect();
+  };
+
   return (
     <>
-      <h2>Hello from {auth.userData.profile.email}</h2>
-      <Button type="primary" danger onClick={() => handleLogout()}>Logout</Button>
+      <Row>
+        <Col span={24}>
+          <h2>Hello from {auth.userData.profile.email}</h2>
+        </Col>
+      </Row>
+      <Row>
+        <Col span={3}>
+          <Button type="primary">Load Weather Data</Button>
+        </Col>
+        <Col span={3}>
+          <Button type="primary" danger onClick={() => handleLogout()}>
+            Logout
+          </Button>
+        </Col>
+      </Row>
     </>
   );
 };
-
